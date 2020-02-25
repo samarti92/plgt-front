40862b0d840d913009201f700427eec0ae5c419e
Santiago Martí <santiago@resit.cl>
2020-02-25 00:48:10 -0300

    chores: update url to heroku, update some names

--- src/containers/redux/service.js
+++ src/containers/redux/service.js
@@ -1,5 +1,5 @@
-//const API_URL = 'https://plgt-backend-rails.herokuapp.com';
-const API_URL = 'http://localhost:3000';
+const API_URL = 'https://plgt-backend-rails.herokuapp.com';
+//const API_URL = 'http://localhost:3000';
 
 export const fetchUsers = () => fetch(`${API_URL}/users.json`);
 
