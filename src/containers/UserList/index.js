ef499a2dca9e985ac01ed4953aa9f302d71d2420
Santiago Martí <santiago@resit.cl>
2020-02-25 00:07:27 -0300

    feat (user list): add redux

--- src/containers/UserList/index.js
+++ src/containers/UserList/index.js
@@ -1,16 +1,22 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchUsers } from '../redux/actions';
+import { Title, Wrapper } from './styles';
 
 import List from '../../components/List';
 
-const UserList = ({ items }) => <List items={items} />;
-
-UserList.propTypes = {
-  items: PropTypes.array
-};
-
-UserList.defaultProps = {
-  items: []
+const UserList = () => {
+  const users = useSelector(state => state.users);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, []);
+  return (
+    <Wrapper className="col-md-6 col-12 container pt-5 card card-body">
+      <Title>Listado</Title>
+      <List items={users} />
+    </Wrapper>
+  );
 };
 
 export default UserList;
